84fc114305da72f1a762a79becb92403bbac8ba4
Utas Prasojo <utasprasojo18@student.uns.ac.id>
2025-05-25 15:44:16 +0700

    Initial clean commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <n-config-provider :theme-overrides="{
+    common: {
+      primaryColor: '#1c2142',
+      primaryColorHover: '#1c2142',
+      primaryColorPressed: '#1c2142',
+    }
+  }" preflight-style-disabled>
+    <router-view />
+  </n-config-provider>
+</template>
---
+++ src/app/page.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+</script>
+<template>
+  example page 1
+</template>
